d6cb0ba73d9971704b8060e92d35adc24a4ccc47
michaelnji <73862378+michaelnji@users.noreply.github.com>
2023-09-11 19:20:58 +0200

    feat: :tada: Beginning of project Bani
    Deleting useless files

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,5 @@
 <script>
-	import Counter from './Counter.svelte';
-	import welcome from '$lib/images/svelte-welcome.webp';
-	import welcome_fallback from '$lib/images/svelte-welcome.png';
+	//
 </script>
 
 <svelte:head>
@@ -9,51 +7,10 @@
 	<meta name="description" content="Svelte demo app" />
 </svelte:head>
 
-<section>
-	<h1>
-		<span class="welcome">
-			<picture>
-				<source srcset={welcome} type="image/webp" />
-				<img src={welcome_fallback} alt="Welcome" />
-			</picture>
-		</span>
-
-		to your new<br />SvelteKit app
-	</h1>
-
-	<h2>
-		try editing <strong>src/routes/+page.svelte</strong>
-	</h2>
-
-	<Counter />
-</section>
-
-<style>
-	section {
-		display: flex;
-		flex-direction: column;
-		justify-content: center;
-		align-items: center;
-		flex: 0.6;
-	}
-
-	h1 {
-		width: 100%;
-	}
-
-	.welcome {
-		display: block;
-		position: relative;
-		width: 100%;
-		height: 0;
-		padding: 0 0 calc(100% * 495 / 2048) 0;
-	}
-
-	.welcome img {
-		position: absolute;
-		width: 100%;
-		height: 100%;
-		top: 0;
-		display: block;
-	}
-</style>
+<div class="max-w-xl mx-auto">
+	<h1 class="font-head text-center text-7xl font-extrabold">Simple notes the Bani way</h1>
+	<p class="mt-2 font-medium text-2xl text-center">
+		Lorem ipsum dolor sit, amet consectetur adipisicing elit. In minima iusto, beatae consequuntur
+		repellat illo?
+	</p>
+</div>
